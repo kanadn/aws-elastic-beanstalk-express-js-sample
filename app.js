18268f3f3ca3467ef7e9f741e69a0aa1b2ffa233
Kanad Naleshwarkar <kanad.naleshwarkar@gmail.com>
2022-05-04 10:03:52 +0530

    Second commit to test the CICD pipeline.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Going to build a continuous delivery pipeline!'));
+app.get('/', (req, res) => res.send('Built a continuous delivery pipeline!... This is the second commit'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
